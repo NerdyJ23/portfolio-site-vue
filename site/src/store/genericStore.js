7a85db5ccabb352210f025e551a2120721b21dfe
NerdyJ23 <jessica.moolenschot@outlook.com>
2025-05-20 09:53:56 +1000

    fix api url

--- site/src/store/genericStore.js
+++ site/src/store/genericStore.js
@@ -1,5 +1,5 @@
 const state = {
-	api: 'https://portfolio.jessica-moolenschot.dev',
+	api: 'https://portfolio.jessica-moolenschot.dev:8080',
 	months: ['January','February','March','April','May','June','July','August','September','October','November','December'],
 	weekday: ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
   }
